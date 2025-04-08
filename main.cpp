0252d50d4c1d15da6154f4fb0a56ab08316e5225
zosqea <zosqea@gmail.com>
2025-04-09 02:13:22 +0300

    Initial commit again

--- main.cpp
+++ main.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
-bool isValidBoard(int *board);
-bool boardGeneration(int *board, int currentIndex, int size);
+class Sudoku{
+		private:
+				int board[81]={};
+				bool isValidBoard();
+				bool generateBoard(int currentIndex);
+		public:
+				Sudoku();
+				void printBoard();
 
+};
+
+Sudoku::Sudoku(){
+		std::srand(std::time(nullptr));
+		generateBoard(0);
+}
 
 int main(){
-		int aboba [81] = {};
-		boardGeneration(aboba,0,81);
-		for(int i = 0; i<9; i++){
-				for(int j = 0; j<9; j++){
-						std::cout<<'['<<aboba[i*9+j]<<']';
-				}
-				std::cout<<'\n';
-		}
+		Sudoku aboba;
+		aboba.printBoard();
 		return 0;
 }
 
-bool isValidBoard(int *board){
+bool Sudoku::isValidBoard(){
 		for(int i = 0; i<9; i++){
 				int counterVertical [10] = {};
 				int counterHorizontal [10] = {};
@@ -33,29 +41,54 @@ bool isValidBoard(int *board){
 						}
 				}
 		}
-		for(int i = 0; i<61; i+=3){
-				int counterSquare [10] = {};
-				for(int j = 0; j<21; j++){
-						int value = board[i+j];
-						if(value){
-								if(counterSquare[value]) return false;
-								counterSquare[value]++;
+		for(int row = 0; row < 9; row+=3){
+				for(int col = 0; col < 9; col+=3){
+						int counterSquare [10] = {};
+						for(int i = 0; i < 3; i++){
+								for(int j = 0; j < 3; j++){
+										int value = board[(row + i)*9 + (col + j)];
+										if(value){
+												if(counterSquare[value]) return false;
+												counterSquare[value]++;
+										}
+								}
 						}
-						if((j+1)%3==0) j+=6;
 				}
-				if((i/3+1)%3==0) i+=18;
 		}
 		return true;
 }
 
-bool boardGeneration(int *board, int currentIndex, int size){
-		if(size==currentIndex) return true; 
-		for(int i = 1; i<10; i++){
-				board[currentIndex]=i;
-				if(isValidBoard(board)){
-						if(boardGeneration(board,currentIndex+1,size)) return true;
+bool Sudoku::generateBoard(int currentIndex){
+		if(currentIndex==81) return true; 
+		int choice[9] = {};
+		int chosen[10] = {};
+		int randomNumber;
+		for(int i = 0; i<9; i++){
+				do{
+						randomNumber = std::rand()%9+1;
+				}while(chosen[randomNumber]);
+				chosen[randomNumber]++;
+				choice[i]=randomNumber;
+		}
+		
+		for(int i = 0; i<9; i++){
+				board[currentIndex]=choice[i];
+				if(isValidBoard()){
+						if(generateBoard(currentIndex+1)) return true;
 				}
 				board[currentIndex]=0;
 		}
-		return false;	
+		return false;
+}
+
+void Sudoku::printBoard(){
+		for(int i = 0; i<9; i++){
+				if(i%3==0) std::cout<<'\n';
+				for(int j = 0; j<9; j++){
+						if(j%3==0) std::cout<<'\t';
+						std::cout<<'['<<board[i*9+j]<<']';
+				}
+				std::cout<<'\n';
+		}
+
 }
